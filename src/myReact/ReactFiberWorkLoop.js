70d1a9047430b92ee56a4e467e2ee1f66c1c0d35
376305 <shishuaiyun@deppon.com>
2024-09-11 11:27:54 +0800

    feat: requestIdleCallback

--- src/myReact/ReactFiberWorkLoop.js
+++ src/myReact/ReactFiberWorkLoop.js
@@ -1,3 +1,25 @@
+
+// wip: work in progress 当前正在工作中的
+let wip = null
+
 export function scheduleUpdateOnFiber(fiber) {
-    console.log(fiber)
+    wip = fiber
 }
+
+// 1. 处理wip
+// 2. 更新wip
+function performUnitWork() {
+    console.log(wip)
+}
+
+function workLoop(IdleDeadLine) {
+    if (IdleDeadLine.timeRemaining() > 0 && wip) {
+        performUnitWork()
+    }
+   
+    // commit阶段：不可中断的
+
+    // requestIdleCallback(workLoop)
+}
+
+requestIdleCallback(workLoop)
