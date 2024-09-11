0f2054b18183ce6db3214cd05580b291dc9317de
376305 <shishuaiyun@deppon.com>
2024-09-11 11:19:26 +0800

    feat: createFiber

--- src/myReact/ReactFiber.js
+++ src/myReact/ReactFiber.js
@@ -1,7 +1,41 @@
-export function createFiber(vnode, returnFiber) {
-  console.log(vnode, returnFiber);
+import { FunctionComponent, HostComponent } from "./ReactWorkTags";
+import { isFn, isStr, Placement } from "./utils";
 
-  const fiber = {};
+export function createFiber(vnode, returnFiber) {
+  const fiber = {
+    // 类型
+    type: vnode.type,
+    key: vnode.key,
+    // 属性
+    props: vnode.props,
+    // 原生标签 dom节点
+    // 函数组件
+    // 类组件 实例
+    stateNode: null,
+
+    // 第一个子节点 fiber
+    child: null,
+    // 下一个兄弟fiber
+    sibling: null,
+    // 父fiber
+    return: returnFiber,
+
+    // 记录位置
+    index: null,
+    // old fiber
+    alternate: null,
+    // 标记fiber effect，比如插入、更新
+    flags: Placement,
+  };
+
+  // 判断组件类型
+  const { type } = vnode;
+  if (isStr(type)) {
+    // 原生标签
+    fiber.tag = HostComponent;
+  } else if (isFn(type)) {
+    fiber.tag = FunctionComponent
+  }
 
   return fiber;
 }
---
+++ src/myReact/utils.js
@@ -0,0 +1,62 @@
+// ! flags
+export const NoFlags = /*      */ 0b00000000000000000000;
+
+// 新增、插入
+export const Placement = /*    */ 0b0000000000000000000010; // 2
+// 节点更新属性
+export const Update = /*       */ 0b0000000000000000000100; // 4
+// 删除 暂时不做 8
+
+export function isStr(s) {
+  return typeof s === "string";
+}
+
+export function isStringOrNumber(s) {
+  return typeof s === "string" || typeof s === "number";
+}
+
+export function isFn(fn) {
+  return typeof fn === "function";
+}
+
+export function isArray(arr) {
+  return Array.isArray(arr);
+}
+
+export function isUndefined(s) {
+  return s === undefined;
+}
+
+// {id: 'red'}
+// {className:'green}
+// fake 合成事件
+export function updateNode(node, prevVal, nextVal) {
+  // 遍历老属性
+  Object.keys(prevVal).forEach((k) => {
+    if (k === "children") {
+      if (isStringOrNumber(nextVal[k])) {
+        node.textContent = "";
+      }
+    } else if (k.slice(0, 2) === "on") {
+      const eventName = k.slice(2).toLocaleLowerCase();
+      node.removeEventListener(eventName, prevVal[k]);
+    } else {
+      if (!(k in nextVal)) {
+        node[k] = "";
+      }
+    }
+  });
+
+  Object.keys(nextVal).forEach((k) => {
+    if (k === "children") {
+      if (isStringOrNumber(nextVal[k])) {
+        node.textContent = nextVal[k];
+      }
+    } else if (k.slice(0, 2) === "on") {
+      const eventName = k.slice(2).toLocaleLowerCase();
+      node.addEventListener(eventName, nextVal[k]);
+    } else {
+      node[k] = nextVal[k];
+    }
+  });
+}
